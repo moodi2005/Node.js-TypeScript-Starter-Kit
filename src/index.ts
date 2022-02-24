bfc389f2a6004c4211e103061c0ed1612820bd08
Mohammad Mahdi Moodi <83002648+moodi2005@users.noreply.github.com>
2022-02-24 23:14:35 +0330

    Creat Web Server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hi ;)');
+import http from'http';
+
+const Port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.end("Hello World By Node");
+})
+
+server.listen(Port,()=>{
+   console.log(`Server is Running To http:localhost:${Port}`)
+});
